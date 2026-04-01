b7933bea379e6243f2f953bdec3e6fc002d95239
Untergeek <untergeek@untergeek.de>
2026-04-01 19:42:29 +0200

    V2 buglist and tech description added§

--- circuitpython/lib/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file marks the 'lib' directory as a Python package.
-# It is intentionally empty.
--- circuitpython/lib/hardware/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file marks the 'hardware' directory as a Python package.
-# It is intentionally empty.
